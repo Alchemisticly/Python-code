b3ce04de486a403673d20ecaba6733b5f5fc3dea
Alchemy <Alchemy@x-xs-macbook.local>
2018-01-16 16:58:29 -0500

    it works but with a glitch I will fix

--- ps0.py
+++ ps0.py
@@ -1,11 +1,11 @@
+#!/usr/bin/python
 # Problem set 0
 # Name: Ian Miner
 # Collaboratoprs: None
 # Time Spent:12 hours
-
 question1 = 'Enter your date of birth'
 x = raw_input('Enter your date of birth')
-print x
 question2 = ('Enter your last name ')
 y = raw_input ('Enter your last name ')
 print (y+x)
+if__name__=='__main__'
