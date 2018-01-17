617cf46269c52b4afcc5308f978c59ded90774d2
Alchemy <Alchemy@x-xs-macbook.local>
2018-01-17 13:26:35 -0500

    removed name== main line and it works now

--- ps0.py
+++ ps0.py
@@ -3,10 +3,9 @@
 # Name: Ian Miner
 # Collaboratoprs: None
 # Time Spent:12 hours
-if__name__=='__main__'
+
 question1 = 'Enter your date of birth'
 x = raw_input('Enter your date of birth')
 question2 = ('Enter your last name ')
 y = raw_input ('Enter your last name ')
 print (y+x)
-    
