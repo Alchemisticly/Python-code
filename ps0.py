c27eb0f9c289b2c7744dd8a6f603df909f80e763
Alchemy <Alchemy@x-xs-macbook.local>
2018-01-17 13:22:48 -0500

    removed code indentation and removed last line

--- ps0.py
+++ ps0.py
@@ -3,9 +3,10 @@
 # Name: Ian Miner
 # Collaboratoprs: None
 # Time Spent:12 hours
+if__name__=='__main__'
 question1 = 'Enter your date of birth'
 x = raw_input('Enter your date of birth')
 question2 = ('Enter your last name ')
 y = raw_input ('Enter your last name ')
 print (y+x)
-if__name__=='__main__'
+    
